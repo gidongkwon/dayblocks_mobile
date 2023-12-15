5071ca5f9e777ba77a12202ebc208435258b400b
Gidong Kwon <lucidfext@gmail.com>
2023-12-15 11:15:04 +0900

    wip

---
+++ lib/src/calendar/calendar_view.dart
@@ -0,0 +1,86 @@
+import 'package:dayblocks_mobile/src/calendar/week_calendar.dart';
+import 'package:dayblocks_mobile/src/task/task_list.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../settings/settings_view.dart';
+import 'sample_item.dart';
+import 'sample_item_details_view.dart';
+
+/// Displays a list of SampleItems.
+class SampleItemListView extends StatelessWidget {
+  const SampleItemListView({
+    super.key,
+    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
+  });
+
+  static const routeName = '/';
+
+  final List<SampleItem> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(DateFormat.yMMMM('ko-KR').format(DateTime.now())),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to the settings page. If the user leaves and returns
+              // to the app after it has been killed while running in the
+              // background, the navigation stack is restored.
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+
+      // To work with lists that may contain a large number of items, it’s best
+      // to use the ListView.builder constructor.
+      //
+      // In contrast to the default ListView constructor, which requires
+      // building all Widgets up front, the ListView.builder constructor lazily
+      // builds Widgets as they’re scrolled into view.
+      body: Stack(children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: WeekCalendar(referenceDate: DateTime.now())
+        ),
+        // ListView.builder(
+        //   // Providing a restorationId allows the ListView to restore the
+        //   // scroll position when a user leaves and returns to the app after it
+        //   // has been killed while running in the background.
+        //   restorationId: 'sampleItemListView',
+        //   itemCount: items.length,
+        //   itemBuilder: (BuildContext context, int index) {
+        //     final item = items[index];
+        //
+        //     return ListTile(
+        //       title: Text('SampleItem ${item.id}'),
+        //       leading: const CircleAvatar(
+        //         // Display the Flutter Logo image asset.
+        //         foregroundImage:
+        //             AssetImage('assets/images/flutter_logo.png'),
+        //       ),
+        //       onTap: () {
+        //         // Navigate to the details page. If the user leaves and returns to
+        //         // the app after it has been killed while running in the
+        //         // background, the navigation stack is restored.
+        //         Navigator.restorablePushNamed(
+        //           context,
+        //           SampleItemDetailsView.routeName,
+        //         );
+        //       });
+        //   },
+        // ),
+        DraggableScrollableSheet(
+          initialChildSize: 0.1,
+          minChildSize: 0.1,
+          maxChildSize: 0.5,
+          builder: (context, scrollController) {
+            return TaskList();
+          })
+      ]));
+  }
+}
